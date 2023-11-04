2b9a351683adb89e287b63b77f26f884ea7e7a82
ThomasRosebrook <trosebr@ksu.edu>
2023-11-04 16:42:07 -0500

    Added Floor

--- HackyGame.cs
+++ HackyGame.cs
@@ -1,18 +1,17 @@
 using Godot;
 using System;
 
-public partial class TheFloor : Area2D
+public partial class HackyGame : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Position = new Vector2(576, 950);
-	}
+        var player = GetNode<Area2D>("Player");
+		player.Position = new Vector2(576, 300);
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print();
-		Position = new Vector2(Position.X, Position.Y - (float)0.1);
 	}
 }
