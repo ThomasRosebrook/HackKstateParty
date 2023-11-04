fdd9c54b094c781e29f4279e07207d91355e0995
Keenan Melton <darkenforcerx@outlook.com>
2023-11-03 23:44:30 -0500

    Menu started?

---
+++ Label.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Label : Godot.Label
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		HorizontalAlignment = HorizontalAlignment.Right;
+		VerticalAlignment = VerticalAlignment.Center;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
