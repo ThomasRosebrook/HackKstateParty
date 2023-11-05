886bcc155ab48f014b30bdf1a25a5789991bce95
ThomasRosebrook <trosebr@ksu.edu>
2023-11-05 01:46:49 -0500

    Fixed Button Issue

--- GameButton2.cs
+++ GameButton2.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class GameButton2 : Godot.Button
+public partial class GameButton2 : Button
 {
 	// Called when the node enters the scene tree for the first time.
 
